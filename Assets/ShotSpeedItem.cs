18282bd3dcb8dd351b10a2d09ff709488c15a7c8
Bailym <bm39@hotmail.co.uk>
2022-07-07 12:21:12 +0100

    add shot speed item

---
+++ Assets/ShotSpeedItem.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotSpeedItem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameObject player = collision.gameObject;
+
+            player.GetComponent<CombatController>().leftBlasterSpeed *= 0.75f;  //decrease shot time by 25 percent
+        }
+    }
+}
